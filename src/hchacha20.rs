94ade65369e15b9920aafaeb5ae761f75daa108e
Calvin Hill <calvin@hakobaito.co.uk>
2018-12-29 01:25:54 +0000

    Add Apache 2.0 License

--- src/hchacha20.rs
+++ src/hchacha20.rs
@@ -1,5 +1,23 @@
-// A pure Rust implementation of HChaCha20 Implemented by Tony Arcieri (tarcieri) <bascule@gmail.com>
-// https://github.com/tendermint/kms/blob/52eb66a317168d617d146b1e1951ec23db7e4e86/src/hchacha20.rs
+/*
+
+Copyright © 2018 Tendermint
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+A pure Rust implementation of HChaCha20 implemented by Tony Arcieri (tarcieri) <bascule@gmail.com>
+https://github.com/tendermint/kms/blob/52eb66a317168d617d146b1e1951ec23db7e4e86/src/hchacha20.rs
+
+*/
 
 #![allow(dead_code)]
 
